5fecd325b13c26b788b49b9f26f49eb8861e2055
Namyunha <108773845+Namyunha@users.noreply.github.com>
2025-03-06 10:18:26 +0900

    chore: 리액트 쿼리 설정정

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import AddComponent from "./_component/AddComponent";
+import BookList from "./_component/BookList";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -10,7 +12,9 @@ const queryClient = new QueryClient();
 export default function Page() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AddComponent />;
+      <AddComponent />
+      <BookList />
+      <ReactQueryDevtools initialIsOpen={true} buttonPosition="bottom-right" />
     </QueryClientProvider>
   );
 }
